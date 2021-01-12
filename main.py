9c93de759bb417293dd731c56512e250f7e48704
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2021-01-12 16:14:23 -0300

    Add working version 1.0

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from telethon import TelegramClient, events, sync
 from dotenv import load_dotenv
 
 
-sleeping_txt = "Estou dormindo, respondo quando acordar "
+sleeping_txt = "I'm sleeping right now, get back to you when I wake up"
 turned_on = False
+already_messaged_users = set()
 
 if __name__ == "__main__":
     load_dotenv(override=True, verbose=True)
@@ -16,19 +17,35 @@ if __name__ == "__main__":
     with TelegramClient(session_name, api_id, api_hash) as client:
         client.send_message('me' , 'BRB online!')
 
-        # /sleep
-        # TODO: fix chat
-        @client.on(events.NewMessage(pattern='(?i)\/sleep$'))
-        async def brb_on_off(event):
-            turned_on = not turned_on
+        # Turns bot on and off
+        async def toggle_brb(event, turn_on):
+            global turned_on
+            turned_on = turn_on
             sender = await event.message.get_sender()
             await event.reply(f"BRB on: {turned_on}")
 
+        # /sleep : turns bot on
+        @client.on(events.NewMessage(pattern='(?i)\/sleep$'))
+        async def enable(event):
+            await toggle_brb(event, True)
+
+        # /up : turns bot off
+        @client.on(events.NewMessage(pattern='(?i)\/up$'))
+        async def disable(event):
+            global already_messaged_users
+            already_messaged_users = set() # clear cache of messaged users
+            await toggle_brb(event, False)
+
         @client.on(events.NewMessage(incoming=True))
         async def say_brb(event):
-            if turned_on == False:
-                await event.reply("Não estou dormindo")
+            global turned_on, already_messaged_users
+            sender = await event.get_sender()
+
+            if not event.is_private or not turned_on or \
+                    sender.id in already_messaged_users:
                 return
+
+            already_messaged_users.add(sender.id)
             await event.reply(sleeping_txt)
 
         client.run_until_disconnected()
